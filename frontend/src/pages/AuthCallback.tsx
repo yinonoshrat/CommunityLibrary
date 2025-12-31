677da0f203f868722192d4db49f7b5fd704361cb
yoshrat <yinon.oshrat@mobileye.com>
2025-12-31 15:18:00 +0200

    trigger build

--- frontend/src/pages/AuthCallback.tsx
+++ frontend/src/pages/AuthCallback.tsx
@@ -55,7 +55,7 @@ export default function AuthCallback() {
       } catch (err: any) {
         console.error('OAuth callback error:', err)
         setError(err.message || 'שגיאה בהתחברות')
-      } 
+      }
     } 
 
     handleCallback()
@@ -81,3 +81,4 @@ export default function AuthCallback() {
     </Container>
   )
 }
+// comment to trigge build
